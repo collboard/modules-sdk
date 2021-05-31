8174dcbce1fba882c9e99469af91e8583039eac2
Pavol Hejny <me@pavolhejny.com>
2021-05-31 14:13:18 +0200

    Making ObservableContentComponent more clean

--- src/colldev/utils/ObservableContentComponent.tsx
+++ src/colldev/utils/ObservableContentComponent.tsx
@@ -7,29 +7,34 @@ import { IAwaitable } from './destroyables/IAwaitable';
  */
 
 interface IObservableContentComponentProps {
-    loading: JSX.Element;
+    /**
+     * Component to be rendered before the content is loaded
+     *
+     * If not set, default loading is used
+     */
+    loading?: JSX.Element;
+
     /**
      * You can put here any RxJS observable. For example BehaviorSubject.
      */
-    content: Observable<IAwaitable<JSX.Element | JSX.Element[]>>;
+    content: Observable<IAwaitable<JSX.Element>>;
 }
 
 interface IObservableContentComponentState {
-    content: JSX.Element | JSX.Element[] | null;
+    content: JSX.Element;
 }
 
-// !! Key prop - is it still relevant
-// !! TODO: Warn when key not used
-// !! TODO: Use keys in usages
-// !! TODO: Probbaly use keys in other async helpers
-
+/**
+ * Utility for mounting RxJS observable content
+ *
+ * @collboard-modules-sdk
+ */
 export function ObservableContentComponent({ loading, content }: IObservableContentComponentProps) {
-    const [state, setState] = React.useState<IObservableContentComponentState>({ content: loading });
+    const [state, setState] = React.useState<IObservableContentComponentState>({ content: loading || <>Loading...</> });
 
     React.useEffect(() => {
         const subscription = content.subscribe(async (newContentAwaitable) => {
             const newContent = await newContentAwaitable;
-            // console.log({ newContent });
             setState({ content: newContent });
         });
 
