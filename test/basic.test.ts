8cdd46555dca1eca615c156fc5d31cfc1cea242d
Pavol Hejný <me@pavolhejny.com>
2021-07-07 10:22:34 +0200

    Basic test mocks

---
+++ test/basic.test.ts
@@ -0,0 +1,7 @@
+//const cmd = require('./cmd');
+describe('the basics', () => {
+    it('should run without crash on the sample module', async () => {
+        //const response = await cmd.execute('', ['--peppers', '--cheese', 'gouda']);
+        //expect(response).to.equal('you ordered a pizza with:\n  - peppers\n  - gouda cheese');
+    });
+});
