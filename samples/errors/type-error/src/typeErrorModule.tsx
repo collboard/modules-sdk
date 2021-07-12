49b62e69bd4ac046ce5d97e21023b79e74e464a8
Pavol Hejny <me@pavolhejny.com>
2021-07-12 17:34:03 +0200

    Add module which has type error

---
+++ samples/errors/type-error/src/typeErrorModule.tsx
@@ -0,0 +1,10 @@
+import { declareModule, makeExtrajsxModule } from '../../../../src/runtime/runtime';
+
+declareModule(
+    makeExtrajsxModule({
+        manifest: {
+            name: 'TypeErrorModule',
+        },
+        // Testing here mising stuff
+    }),
+);
