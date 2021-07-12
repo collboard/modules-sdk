3a9f244a27a74d23c57e06f8770404d50c7361fb
Pavol Hejny <me@pavolhejny.com>
2021-07-12 17:34:21 +0200

    Add basic sample module

---
+++ samples/hello-world/src/sampleModule.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { declareModule, ExtraJsxPlace, makeExtrajsxModule } from '../../../src/runtime/runtime';
+
+declareModule(
+    makeExtrajsxModule({
+        manifest: {
+            name: 'SampleColldevModule',
+            title: { en: 'Hello world' },
+            description: {
+                en: 'Simple sample hello world button for Collboard.com',
+            },
+            keywords: [],
+            categories: ['Productivity', 'Buttons', 'Template'],
+            icon: '/assets/icons/copy-material.svg',
+        },
+        place: ExtraJsxPlace.EdgeRight,
+        createExtraJsx() {
+            return (
+                <button
+                    onClick={async () => {
+                        alert(`Hello from testing of Collboard modules SDK!`);
+                    }}
+                    className="button button-primary button-vertical"
+                >
+                    <span>🥑 Hello World!</span>
+                </button>
+            );
+        },
+    }),
+);
