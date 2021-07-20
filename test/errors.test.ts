d1bf22d5207bd577feb7e9ac2786ca371963316a
Pavol Hejny <me@pavolhejny.com>
2021-07-20 13:04:13 +0200

    Passing all the tests

--- test/errors.test.ts
+++ test/errors.test.ts
@@ -6,10 +6,6 @@ import { getFreshPortflag } from './utils/getFreshPortFlag';
 jest.setTimeout(1000 * 60);
 
 describe('the errored modules', () => {
-    it('xxxxxxx', async () => {
-        console.log(await getCollboardUrlFlag());
-    });
-
     /*/
     it('should PASS when testing exit codes', async () => {
         await expect(
@@ -32,10 +28,17 @@ describe('the errored modules', () => {
     });
     /**/
 
+    async function createFlags() {
+        const popupTestingBrowser = true;
+        return `--open multiple --exit --disconnect ${
+            popupTestingBrowser ? '' : '--headless'
+        } ${await getFreshPortflag()} ${await getCollboardUrlFlag()}`;
+    }
+
     it(`should NOT crash when there is NO ERROR in the module`, async () => {
         await expect(
             execCommand({
-                command: `ts-node ./src/colldev/main.ts develop ./test-samples/errors/no-error --open multiple --exit  --disconnect ${await getFreshPortflag()} ${await getCollboardUrlFlag()}`,
+                command: `ts-node ./src/colldev/main.ts develop ./test-samples/errors/no-error ${await createFlags()}`,
                 cwd: join(__dirname, '..'),
             }),
         ).resolves.not.toThrow();
@@ -55,7 +58,7 @@ describe('the errored modules', () => {
         it(`should crash when there is a ${errorType.split('-').join(' ').toUpperCase()} in the module`, async () => {
             await expect(
                 execCommand({
-                    command: `ts-node ./src/colldev/main.ts develop ./test-samples/errors/${errorType} --open multiple --exit --headless -c --disconnect ${await getFreshPortflag()}  ${await getCollboardUrlFlag()}`,
+                    command: `ts-node ./src/colldev/main.ts develop ./test-samples/errors/${errorType} ${await createFlags()}`,
                     cwd: join(__dirname, '..'),
                 }),
             ).rejects.toThrow();
