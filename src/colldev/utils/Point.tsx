4929722515aedf38dffd529d649609066c00181e
Pavol Hejny <me@pavolhejny.com>
2021-09-04 05:57:00 +0200

    Utility for showing checkpoints

---
+++ src/colldev/utils/Point.tsx
@@ -0,0 +1,25 @@
+import { Text } from 'ink';
+import Spinner from 'ink-spinner';
+import * as React from 'react';
+
+export function Point({ children, done, failed }: React.PropsWithChildren<{ done?: boolean; failed?: boolean }>) {
+    if (done) {
+        return (
+            <Text color="green" bold>
+                [✔] {children}
+            </Text>
+        );
+    } else if (failed) {
+        return (
+            <Text color="red" bold>
+                [✗] {children}
+            </Text>
+        );
+    } else {
+        return (
+            <Text color="yellow" bold>
+                [<Spinner type="arrow" />] {children}
+            </Text>
+        );
+    }
+}
