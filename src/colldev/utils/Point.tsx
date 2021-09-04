81adb017eb9ff1d169993d7f985144a170bd5ced
Pavol Hejny <me@pavolhejny.com>
2021-09-04 14:19:25 +0200

    Point can be errored and skipped

--- src/colldev/utils/Point.tsx
+++ src/colldev/utils/Point.tsx
@@ -5,19 +5,19 @@ import * as React from 'react';
 export function Point({
     children,
     done,
-    failed,
+    error,
     skipped,
-}: React.PropsWithChildren<{ done?: boolean; failed?: boolean; skipped?: boolean }>) {
-    if (done) {
-        return (
-            <Text color="green" bold>
-                [✔] {children}
-            </Text>
-        );
-    } else if (failed) {
+}: React.PropsWithChildren<{ done?: boolean; error?: boolean; skipped?: boolean }>) {
+    if (error) {
         return (
             <Text color="red" bold>
                 [✗] {children}
+            </Text>
+        );
+    } else if (done) {
+        return (
+            <Text color="green" bold>
+                [✔] {children}
             </Text>
         );
     } else if (skipped) {
