8e14b690a0b4888f43091032a735f5dcc09e993e
Pavol Hejny <me@pavolhejny.com>
2021-12-01 19:10:57 +0100

    Publishing is still in private beta.

--- src/colldev/commands/publish/ColldevPublish.tsx
+++ src/colldev/commands/publish/ColldevPublish.tsx
@@ -2,7 +2,7 @@ import commander from 'commander';
 import { Destroyable } from 'destroyable';
 import { Box, Text } from 'ink';
 import * as React from 'react';
-import { ProductionCompiler } from "../../services/Compiler/ProductionCompiler";
+import { ProductionCompiler } from '../../services/Compiler/ProductionCompiler';
 import { ICommand } from '../ICommand';
 import { IColldevPublishOptions } from './IColldevPublishOptions';
 import { IColldevPublishStatus } from './IColldevPublishStatus';
@@ -26,16 +26,16 @@ export class ColldevPublish extends Destroyable implements ICommand<IColldevPubl
     public render() {
         return (
             <Box borderStyle="round" display="flex" flexDirection="column" borderColor="red">
-                <Text color="red">We are still working on automatical deployment </Text>
-                <Text>Please send us your work to dev@collboard.com</Text>
+                <Text color="red">Publishing is still in private beta.</Text>
+                <Text>Please contact us on dev@collboard.com to get publishing token.</Text>
             </Box>
         );
     }
 
     public status() {
         return {
-            error: `We are still working on automatical deployment`,
-            details: `Please send us your work to dev@collboard.com`,
+            error: `Publishing is still in private beta.`,
+            details: `Please contact us on dev@collboard.com to get publishing token.`,
         };
     }
 }
