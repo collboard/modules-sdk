fd6907d92a0a739fa10d9721cdb4f693acbcfd52
Pavol Hejny <me@pavolhejny.com>
2021-12-04 12:13:49 +0100

    Empty sample

---
+++ test-samples/errors/empty-project/src/neverDeclaredModule.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { declareModule, ExtraJsxPlace, makeExtrajsxModule } from '../../../../src/runtime/runtime';
+
+const makedModule = makeExtrajsxModule({
+    manifest: {
+        name: 'never-declared-module',
+        title: { en: 'Hello world' },
+        description: {
+            en: 'Simple sample hello world button for Collboard.com',
+        },
+        keywords: [],
+        categories: ['Productivity', 'Buttons', 'Template'],
+        icon: '/assets/icons/copy-material.svg',
+    },
+    place: ExtraJsxPlace.EdgeRight,
+    createExtraJsx() {
+        return (
+            <button className="button button-primary button-vertical">
+                <div>Nothing happen here</div>
+            </button>
+        );
+    },
+});
+
+// Do not run declareModule with makedModule
+makedModule;
+declareModule;
