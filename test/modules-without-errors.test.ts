6d62b095c41a742328d71dc74a34ae112cffc9c1
Pavol Hejny <me@pavolhejny.com>
2022-01-06 13:41:50 +0100

    Maybe TODO: Work when running colldev without path only in the current directory

--- test/modules-without-errors.test.ts
+++ test/modules-without-errors.test.ts
@@ -3,6 +3,19 @@ import { createModuleDevelopTest } from './test-utils/createModuleDevelopTest';
 jest.setTimeout(60 * 1000);
 
 describe('misc working modules', () => {
+    /* Maybe TODO: 
+    it(`should work when running colldev without path only in the current directory`, () =>
+        expect(
+            getFlags().then((flags) =>
+                execCommand({
+                    command: `npx ts-node --project ../../tsconfig.json ../../src/colldev/main.ts develop ${flags}`,
+                    cwd: join(__dirname, '../../test-samples/hello-world'),
+                    timeout: 60 * 1000,
+                }),
+            ),
+        ).resolves.not.toThrowError());
+    */
+
     it(`should work when there is no error in the module`, createModuleDevelopTest('errors/no-error', null));
     it(`should work on hello-world sample`, createModuleDevelopTest('hello-world', null));
     it(`should work on multimodule sample`, createModuleDevelopTest('multimodule', null));
