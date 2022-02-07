3ba3785215530153e11be9f3710f0a0cab83cfb8
Pavol Hejny <me@pavolhejny.com>
2022-02-07 12:44:39 +0100

    Colldev working even if webpack stats are not available or broken

--- src/colldev/services/Compiler/CompilerStatusOutputComponent.tsx
+++ src/colldev/services/Compiler/CompilerStatusOutputComponent.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from 'ink';
 import useStdoutDimensions from 'ink-use-stdout-dimensions';
 import * as React from 'react';
+import spaceTrim from 'spacetrim';
 import { Bullet } from '../../utils/Bullet';
 import { ErrorComponent } from '../../utils/ErrorComponent';
 import { ICompilerStatus } from './ICompilerStatus';
@@ -27,11 +28,25 @@ export function CompilerStatusOutputComponent({
 
     const [columns, rows] = useStdoutDimensions();
 
-    let webpackStatsString =
-        webpackStats?.toString({
-            chunks: false, // Makes the build much quieter
-            colors: true, // Shows colors in the console
-        }) || 'Compiled successfully';
+    let webpackStatsString: string;
+    if (!webpackStats) {
+        webpackStatsString = spaceTrim(`
+          No Weback stats available but seems that module is
+          Compiled successfully
+        `);
+    } else {
+        try {
+            webpackStatsString = webpackStats.toString({
+                chunks: false, // Makes the build much quieter
+                colors: true, // Shows colors in the console
+            });
+        } catch (error) {
+            webpackStatsString = spaceTrim(`
+              Can not get Weback stats but seems that module is
+              Compiled successfully
+            `);
+        }
+    }
 
     if (rows < 25) {
         // Note: Showing only last line when compacted
