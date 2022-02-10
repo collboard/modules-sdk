ffb484896ee26682914a0c724397223e6e323059
Pavol Hejny <me@pavolhejny.com>
2022-02-10 21:33:55 +0100

    "⏫ Update by generate-modules-sdk"

---
+++ types/40-utils/logger/LoggerWithInitialize.d.ts
@@ -0,0 +1,24 @@
+// GENERATED WITH generate-modules-sdk
+// Warning: Do not edit by hand, all changes will be lost on next execution!
+// TODO: This file should be excluded from (not only VSCode) auto-importing.
+//       @see https://github.com/Microsoft/vscode/issues/40248
+//       @see https://github.com/microsoft/TypeScript/issues/35395
+//       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
+import { ILogger, ISubLogger } from './ILogger';
+/**
+ * LoggerWithInitialize will call initialize callback once before first action into the logger.
+ * Note: If any logging called, initialize will not be ever called
+ */
+export declare class LoggerWithInitialize implements ILogger {
+    private readonly initialize;
+    private readonly logger;
+    constructor(initialize: () => void, logger: ILogger);
+    private initialized;
+    private initializeOnce;
+    info(message?: any, ...optionalParams: any[]): this;
+    warn(warning?: any, ...optionalParams: any[]): this;
+    error(error?: Error | any, ...optionalParams: any[]): this;
+    trace(...data: any[]): this;
+    group(groupName?: string, ...optionalParams: any[]): ISubLogger;
+    groupCollapsed(groupName?: string, ...optionalParams: any[]): ISubLogger;
+}
