372e3aef4bdc0cd68b126bc1541be1b6c519aa1a
Pavol Hejny <me@pavolhejny.com>
2022-04-15 21:42:04 +0200

    Correct date in tmp folders

--- src/colldev/utils/getUniqueFoldername.ts
+++ src/colldev/utils/getUniqueFoldername.ts
@@ -3,5 +3,7 @@ import { string_folder_relative_path } from '../../../types';
 
 export function getUniqueFoldername(): string_folder_relative_path {
     const d = new Date();
-    return `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}--${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}--${uuid.v4()}`;
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${
+        d.getDate() + 1
+    }--${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}--${uuid.v4()}`;
 }
