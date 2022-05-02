48df991896e10f78f06bdecb048123f6a6a5f106
Pavol Hejny <me@pavolhejny.com>
2022-05-02 22:49:27 +0200

    "⏫ Update by generate-modules-sdk"

---
+++ types/40-utils/logger/ConsoleGroupSubLogger.d.ts
@@ -0,0 +1,16 @@
+// 🏭 GENERATED WITH generate-modules-sdk
+// ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+// TODO: This file should be excluded from (not only VSCode) auto-importing.
+//       @see https://github.com/Microsoft/vscode/issues/40248
+//       @see https://github.com/microsoft/TypeScript/issues/35395
+//       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
+import { ISubLogger } from './ILogger';
+import { RecordLogger } from './RecordLogger';
+export declare class ConsoleGroupSubLogger extends RecordLogger implements ISubLogger {
+    private readonly console;
+    private readonly collapsed;
+    private readonly groupName?;
+    private readonly optionalParams;
+    constructor(console: Console, collapsed: boolean, groupName?: string | undefined, ...optionalParams: any[]);
+    end(): void;
+}
--- types/40-utils/logger/ConsolePrefixLogger.d.ts
+++ types/40-utils/logger/ConsolePrefixLogger.d.ts
@@ -4,12 +4,11 @@
 //       @see https://github.com/Microsoft/vscode/issues/40248
 //       @see https://github.com/microsoft/TypeScript/issues/35395
 //       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
-import { ConsolePrefixLogger } from './ConsolePrefixLogger';
 import { ILogger, ISubLogger } from './ILogger';
-export declare class ConsoleLogger implements ILogger {
+export declare class ConsolePrefixLogger implements ILogger, ISubLogger {
     private readonly console;
-    static readonly console: ConsoleLogger;
-    constructor(console: Console);
+    private readonly prefix;
+    constructor(console: Console, prefix?: string, ...optionalParams: any[]);
     info(message?: any, ...optionalParams: any[]): this;
     warn(warning?: any, ...optionalParams: any[]): this;
     error(error?: Error | any, ...optionalParams: any[]): this;
@@ -17,12 +16,9 @@ export declare class ConsoleLogger implements ILogger {
     group(groupName?: string, ...optionalParams: any[]): ISubLogger;
     groupCollapsed(groupName?: string, ...optionalParams: any[]): ISubLogger;
     groupPrefixed(groupName?: string, ...optionalParams: any[]): ConsolePrefixLogger;
+    /**
+     * Note: In PrefixLogger does nothing
+     *
+     */
+    end(): void;
 }
-/**
- * TODO: Maybe split into multiple files
- * TODO: Nicer begin / end messages
- * TODO: Better Graphics of the tree:
- *       > console.log('%c Oh my heavens! ', 'border-left: 30px solid red');console.log('%c Oh my heavens! ', 'border-left: 30px solid red');
- * TODO: Multiple layers
- *       > const gg = g.groupPrefixed();
- */
