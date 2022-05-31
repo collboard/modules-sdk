02a25d59825300365ad93a5ac083d549d068e83e
Pavol Hejny <me@pavolhejny.com>
2022-05-31 23:30:02 +0200

    Fix types

--- src/colldev/utils/factor.ts
+++ src/colldev/utils/factor.ts
@@ -1,6 +1,6 @@
-import { IFactorable } from '../../../types';
+import { Factorable } from '../../../types';
 
-export function factor<T>(factorable: IFactorable<T>): T {
+export function factor<T>(factorable: Factorable<T>): T {
     if (typeof factorable === 'function') {
         return (factorable as any)();
     } else {
