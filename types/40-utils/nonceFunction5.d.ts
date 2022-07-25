7d61a650bd790d892d413a7a6bef57eff93b4380
Pavol Hejny <me@pavolhejny.com>
2022-07-25 15:47:47 +0200

    ⏫ Update by generate-modules-sdk
    🏭 This commit was automatically generated by generate-modules-sdk script.

---
+++ types/40-utils/nonceFunction5.d.ts
@@ -0,0 +1,13 @@
+// 🏭 GENERATED WITH generate-modules-sdk
+// ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+// TODO: This file should be excluded from (not only VSCode) auto-importing.
+//       @see https://github.com/Microsoft/vscode/issues/40248
+//       @see https://github.com/microsoft/TypeScript/issues/35395
+//       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
+/**
+ * This function does nothing. Just return 5.
+ * Its purpose is to be used as a tester of compatibility between SDK versions.
+ *
+ * @collboard-modules-sdk
+ */
+export declare function nonceFunction5(): number;
