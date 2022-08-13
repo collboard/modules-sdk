c4be0b3ae0fed10ed2a25210dd9c65b8a78ead71
Pavol Hejny <me@pavolhejny.com>
2022-08-13 23:40:03 +0200

    Update the Jest + fix tests

--- test/test-utils/createModuleDevelopTest.ts
+++ test/test-utils/createModuleDevelopTest.ts
@@ -2,28 +2,28 @@ import { join } from 'path';
 import { execCommand } from './execCommand/execCommand';
 import { getFlags } from './getFlags';
 
-export function createModuleDevelopTest(modulePath: string, expectError: string | null): any {
-    if (!expectError) {
-        return () =>
-            expect(
-                getFlags().then((flags) =>
-                    execCommand({
-                        command: `npx ts-node ./src/colldev/main.ts develop ./test-samples/${modulePath} ${flags}`,
-                        cwd: join(__dirname, '../..'),
-                        timeout: 60 * 1000,
-                    }),
-                ),
-            ).resolves.not.toThrowError();
-    } else {
-        return () =>
-            expect(
-                getFlags().then((flags) =>
-                    execCommand({
-                        command: `npx ts-node ./src/colldev/main.ts develop ./test-samples/${modulePath} ${flags}`,
-                        cwd: join(__dirname, '../..'),
-                        timeout: 60 * 1000,
-                    }),
-                ),
-            ).rejects.toThrowError(expectError);
-    }
+export function createModuleDevelopTest(modulePath: string, expectError: string | null): () => void {
+    return () => {
+        const expectation = expect(
+            getFlags().then((flags) => {
+                const command = `npx ts-node --openssl-legacy-provider ./src/colldev/main.ts develop ./test-samples/${modulePath} ${flags}`;
+                const cwd = join(__dirname, '../..');
+                const timeout = 60 * 1000;
+
+                // console.log({ command, flags, cwd, timeout });
+
+                return execCommand({
+                    command,
+                    cwd,
+                    timeout,
+                });
+            }),
+        );
+
+        if (!expectError) {
+            expectation.resolves.not.toThrowError();
+        } else {
+            expectation.rejects.toThrowError(expectError);
+        }
+    };
 }
