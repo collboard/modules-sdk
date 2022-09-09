d863cf94ece686c59d6b4c0509a09143dddb610e
Pavol Hejny <me@pavolhejny.com>
2022-09-09 16:40:35 +0200

    ⏫ Update by generate-modules-sdk
    🏭 This commit was automatically generated by generate-modules-sdk script.

--- types/40-utils/logger/RecordLogger.d.ts
+++ types/40-utils/logger/RecordLogger.d.ts
@@ -5,24 +5,25 @@
 //       @see https://github.com/microsoft/TypeScript/issues/35395
 //       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
 import { ILogger, ISubLogger } from './ILogger';
-interface IRecordLoggerLog {
+export interface IRecordLoggerLog {
     type: 'INFO' | 'WARN' | 'ERROR' | 'TRACE';
     message: any;
     optionalParams: Array<any>;
 }
 export declare class RecordLogger implements ILogger, ISubLogger {
     private _logs;
+    private _isEnded;
     get logsStructured(): Array<IRecordLoggerLog>;
     get logs(): string;
     info(message?: any, ...optionalParams: Array<any>): this;
     warn(message?: any, ...optionalParams: Array<any>): this;
     error(message?: any, ...optionalParams: Array<any>): this;
     trace(...data: Array<any>): this;
+    get isEnded(): boolean;
     end(): void;
     group(groupName?: string, ...optionalParams: Array<any>): ISubLogger;
     groupCollapsed(groupName?: string, ...optionalParams: Array<any>): ISubLogger;
 }
-export {};
 /**
  * TODO: Probably rename logs to logsText and logsStructured to logs
  * TODO: Better support for the groups
