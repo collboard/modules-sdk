fb3851f9dfae7fed145eb516c52425478c744da5
Pavol Hejny <me@pavolhejny.com>
2023-11-29 15:45:35 +0100

    ⏫ Update by generate-modules-sdk
    🏭 This commit was automatically generated by generate-modules-sdk script.

---
+++ types/72-tools/WhichArtTool.d.ts
@@ -0,0 +1,15 @@
+// 🏭 GENERATED WITH generate-modules-sdk
+// ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+// TODO: This file should be excluded from (not only VSCode) auto-importing.
+//       @see https://github.com/Microsoft/vscode/issues/40248
+//       @see https://github.com/microsoft/TypeScript/issues/35395
+//       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
+/**
+ * TODO: [🔅] Put all %c styled console logs styles into one config place
+ */
+export declare const WHICH_ARTS_STYLE =
+    'background: #66ffff; color: black; font-size: 1.1em; font-weight: bold; padding: 5px; border-radius: 3px;';
+/**
+ * TODO: [🏰] We want to be able selecting, copying, exporting, etc. arts in view mode
+ *       Now it is messed by the selection which is always behaving like with edit capabilities
+ */
