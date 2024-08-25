73f5ec5c9ddd886ec5e90a1cd4faa6e8e67138de
Pavol Hejny <me@pavolhejny.com>
2024-08-25 17:29:46 +0200

    ⏫ Update by generate-modules-sdk
    🏭 This commit was automatically generated by generate-modules-sdk script.

--- types/50-systems/ModuleStore/utils/parseKeywordsFromManifest.d.ts
+++ types/50-systems/ModuleStore/utils/parseKeywordsFromManifest.d.ts
@@ -4,7 +4,7 @@
 //       @see https://github.com/Microsoft/vscode/issues/40248
 //       @see https://github.com/microsoft/TypeScript/issues/35395
 //       @see https://stackoverflow.com/questions/47796545/how-to-disable-auto-import-from-specific-files-in-vscode
-import { IKeywords } from 'n12';
+import { IKeywords } from '@promptbook/utils';
 import { IModuleManifest } from '../interfaces/IModuleManifest/IModuleManifest';
 /**
  * Create list of keywords from module manifest
